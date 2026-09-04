f4072cbfe2d30043ee36a0c464723fda57cf468e
rufino_fg2 <rufinofigueroa513@gmail.com>
2026-09-04 02:53:59 -0300

    fix in repository

--- src/main/java/com/example/GymProyect/client/repository/ClientRepository.java
+++ src/main/java/com/example/GymProyect/client/repository/ClientRepository.java
@@ -1,7 +1,7 @@
 package com.example.GymProyect.client.repository;
 
 import com.example.GymProyect.client.entity.Cliente;
-import org.hibernate.internal.util.Optional;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -14,13 +14,13 @@ public interface ClientRepository extends JpaRepository<Cliente, Long> {
 
     boolean existsByDni(String dni);
 
-    Optional<Cliente> findByPhone_number(String phone_number);
+    Optional<Cliente> findByPhoneNumber(String phoneNumber);  // Changed from phone_number
 
-    boolean existsByPhone_number(String phone_number);
+    boolean existsByPhoneNumber(String phoneNumber);  // Changed from phone_number
 
     List<Cliente> findByNameContainingIgnoreCase(String name);
 
     List<Cliente> findByLastnameContainingIgnoreCase(String lastname);
 
-    Optional<Cliente> findByUser_Id(Long id);
+    Optional<Cliente> findByUserId(Long id);  // Changed from User_Id
 }

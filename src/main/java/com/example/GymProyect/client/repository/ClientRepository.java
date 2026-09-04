bf722b998149b5378c7cd840d6235a6df743ff1a
rufino_fg2 <rufinofigueroa513@gmail.com>
2026-09-04 02:49:30 -0300

    adding method in repository

--- src/main/java/com/example/GymProyect/client/repository/ClientRepository.java
+++ src/main/java/com/example/GymProyect/client/repository/ClientRepository.java
@@ -1,9 +1,26 @@
 package com.example.GymProyect.client.repository;
 
 import com.example.GymProyect.client.entity.Cliente;
+import org.hibernate.internal.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ClientRepository extends JpaRepository<Cliente, Long> {
+
+    Optional<Cliente> findByDni(String dni);
+
+    boolean existsByDni(String dni);
+
+    Optional<Cliente> findByPhone_number(String phone_number);
+
+    boolean existsByPhone_number(String phone_number);
+
+    List<Cliente> findByNameContainingIgnoreCase(String name);
+
+    List<Cliente> findByLastnameContainingIgnoreCase(String lastname);
+
+    Optional<Cliente> findByUser_Id(Long id);
 }

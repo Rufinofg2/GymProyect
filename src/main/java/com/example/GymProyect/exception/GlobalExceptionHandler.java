24b0aa1f25cbff2dcaf78b1e9abc314093f6dbfb
Rufino Figueroa <135924127+Rufinofg2@users.noreply.github.com>
2026-09-05 22:47:10 -0300

    Fix: Improve code formatting and method naming in GlobalExceptionHandler

--- src/main/java/com/example/GymProyect/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/GymProyect/exception/GlobalExceptionHandler.java
@@ -15,7 +15,7 @@ import java.time.LocalDateTime;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(UserNotFound.class)
-    public ResponseEntity<ErrorResponse> handleUserNotFound(UserNotFound ex, HttpServletRequest request){
+    public ResponseEntity<ErrorResponse> handleUserNotFound(UserNotFound ex, HttpServletRequest request) {
         ErrorResponse error = ErrorResponse.builder()
                 .status(HttpStatus.NOT_FOUND.value())
                 .error(HttpStatus.NOT_FOUND.getReasonPhrase())
@@ -27,7 +27,7 @@ public class GlobalExceptionHandler {
     }
 
     @ExceptionHandler(EmailAlreadyExists.class)
-    public ResponseEntity<ErrorResponse> handleEmailAlreadyExists(EmailAlreadyExists ex, HttpServletRequest request){
+    public ResponseEntity<ErrorResponse> handleEmailAlreadyExists(EmailAlreadyExists ex, HttpServletRequest request) {
         ErrorResponse error = ErrorResponse.builder()
                 .status(HttpStatus.BAD_REQUEST.value())
                 .error(HttpStatus.BAD_REQUEST.getReasonPhrase())
@@ -39,7 +39,7 @@ public class GlobalExceptionHandler {
     }
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<ErrorResponse>handlergenericException(Exception ex,HttpServletRequest request){
+    public ResponseEntity<ErrorResponse> handlerGenericException(Exception ex, HttpServletRequest request) {
         ErrorResponse error = ErrorResponse.builder()
                 .status(HttpStatus.INTERNAL_SERVER_ERROR.value())
                 .error(HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase())
